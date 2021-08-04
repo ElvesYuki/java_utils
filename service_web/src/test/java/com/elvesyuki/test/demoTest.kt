fa1d7c1f95327ccad8219d938d9a6ec4c2e3564e
luohuan <1026770043@qq.com>
2021-08-04 15:22:10 +0800

    feat: Kotlin依赖

---
+++ service_web/src/test/java/com/elvesyuki/test/demoTest.kt
@@ -0,0 +1,16 @@
+package com.elvesyuki.test
+
+
+
+/**
+ * @ClassName AdminOrgController
+ * @Description
+ * @Author luohuan
+ * @Date 2021/8/4 下午3:02
+ */
+
+fun main(args: Array<String>) {    // 包级可见的函数，接受一个字符串数组作为参数
+
+    System.out.println("Hello World!")
+
+}
