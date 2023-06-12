39002f6dd652d4349f28fcfdc78066ea8cd9a0c3
luohuan <luohuan@rocpow.com>
2023-06-12 16:56:23 +0800

    feat: kotlin测试

---
+++ normal_utils/src/main/java/com/elvesyuki/javautils/normal/utils/WebSocketServer.java
@@ -0,0 +1,59 @@
+package com.elvesyuki.javautils.normal.utils;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import static org.apache.commons.net.ftp.FTPCommand.PORT;
+
+/**
+ * @author ：luohuan
+ * @date ：Created in 2023/5/16 15:49
+ * @description：
+ * @modified By：
+ */
+public class WebSocketServer {
+
+
+    public void init () throws InterruptedException {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline p = ch.pipeline();
+                            //p.addLast(new LoggingHandler(LogLevel.INFO));
+                        }
+                    });
+
+            // Start the server.
+            ChannelFuture f = b.bind(PORT).sync();
+
+            // Wait until the server socket is closed.
+            f.channel().closeFuture().sync();
+        } finally {
+            // Shut down all event loops to terminate all threads.
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+
+
+    }
+
+
+
+
+}
